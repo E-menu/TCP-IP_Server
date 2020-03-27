da336d996c00fa02558058a660a4ee6ce934cdfc
Przemyslaw Malec <p.malec@caisson.pl>
2020-03-27 19:07:46 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Server_TCP_IP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+namespace Server_TCP_IP
+{
+    class Server
+    {
+
+
+
+
+    }
+}
